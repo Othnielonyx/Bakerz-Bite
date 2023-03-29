af4510f7d468f9b05a13afcfed811df82339e26b
Lekan <jimoridwanlekan@gmail.com>
2023-03-29 16:08:32 +0100

    display product page

--- bakers/src/app/product/product.component.ts
+++ bakers/src/app/product/product.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { Product, PRODUCTS } from './products';
+
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
 export class ProductComponent {
-
+  products: Product[] = PRODUCTS;
 }
---
+++ bakers/src/app/product/products.ts
@@ -0,0 +1,73 @@
+export interface Product {
+  name: string;
+  price: number;
+  description: string;
+  // imageName: string;
+  path: string;
+}
+
+export const PRODUCTS: Product[] = [
+  {
+    name: 'Product 1',
+    price: 10.99,
+    description: 'This is the description of product 1.',
+    // imageName: 'product1.jpg',
+    path: '../../assets/images/product1.jpg',
+  },
+  {
+    name: 'Product 2',
+    price: 20.99,
+    description: 'This is the description of product 2.',
+    // imageName: 'product2.jpg',
+    path: '../../assets/images/product2.jpg',
+  },
+  {
+    name: 'Product 3',
+    price: 30.99,
+    description: 'This is the description of product 3.',
+    // imageName: 'product3.jpg',
+    path: '../../assets/images/product3.jpg',
+  },
+  {
+    name: 'Product 4',
+    price: 40.99,
+    description: 'This is the description of product 4.',
+    // imageName: 'product4.jpg',
+    path: '../../assets/images/product4.jpg',
+  },
+  {
+    name: 'Product 5',
+    price: 50.99,
+    description: 'This is the description of product 5.',
+    // imageName: 'product5.jpg',
+    path: '../../assets/images/product5.jpg',
+  },
+  {
+    name: 'Product 6',
+    price: 60.99,
+    description: 'This is the description of product 6.',
+    // imageName: 'product6.jpg',
+    path: '../../assets/images/product6.jpg',
+  },
+  {
+    name: 'Product 7',
+    price: 70.99,
+    description: 'This is the description of product 7.',
+    // imageName: 'product7.jpg',
+    path: '../../assets/images/product7.jpg',
+  },
+  {
+    name: 'Product 8',
+    price: 80.99,
+    description: 'This is the description of product 8.',
+    // imageName: 'product8.jpg',
+    path: '../../assets/images/product8.jpg',
+  },
+  {
+    name: 'Product 9',
+    price: 90.99,
+    description: 'This is the description of product 9.',
+    // imageName: 'product9.jpg',
+    path: '../../assets/images/product9.jpg',
+  },
+];
